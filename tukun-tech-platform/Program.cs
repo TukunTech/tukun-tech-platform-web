f5a0765ffc244b4eedfec8f1ad3f06172eea7a9b
Oscar <oscaredu01@hotmail.com>
2024-10-27 17:36:23 -0500

    feat(open-api): enable open api annotations.

--- tukun-tech-platform/Program.cs
+++ tukun-tech-platform/Program.cs
@@ -1,25 +1,61 @@
+using Microsoft.EntityFrameworkCore;
+using tukun_tech_platform.Shared.Domain.Repositories;
+using tukun_tech_platform.Shared.Infrastructure.EFC.Configuration;
+using tukun_tech_platform.Shared.Infrastructure.EFC.Repositories;
+using tukun_tech_platform.Shared.Infrastructure.Interfaces.ASP.Configuration;
+using tukun_tech_platform.Tukun.Application.Internal.CommandServices.Doctors;
+using tukun_tech_platform.Tukun.Application.Internal.QueryServices.Doctors;
+using tukun_tech_platform.Tukun.Domain.Repositories.Doctors;
+using tukun_tech_platform.Tukun.Domain.Services.Doctors;
+using tukun_tech_platform.Tukun.Infrastructure.Repositories.Doctors;
+
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-
-builder.Services.AddControllers();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddControllers(options => options.Conventions.Add(new
+    KebabCaseRouteNamingConvention()));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen(options => options.EnableAnnotations());
+//DB
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+if (connectionString is null)
+    throw new Exception("Database connection string not found");
+if (builder.Environment.IsDevelopment())
+    builder.Services.AddDbContext<AppDbContext>(
+        options =>
+        {
+            options.UseMySQL(connectionString)
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+        });
+else if (builder.Environment.IsProduction())
+    builder.Services.AddDbContext<AppDbContext>(
+        options =>
+        {
+            options.UseMySQL(connectionString)
+                .LogTo(Console.WriteLine, LogLevel.Error)
+                .EnableDetailedErrors();
+        });
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
+builder.Services.AddScoped<IDoctorCommandService, DoctorCommandService>();
+builder.Services.AddScoped<IDoctorQueryService, DoctorQueryService>();
 var app = builder.Build();
-
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
