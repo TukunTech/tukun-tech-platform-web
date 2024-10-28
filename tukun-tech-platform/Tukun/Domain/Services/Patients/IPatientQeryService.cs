439cfa87b62432e5152f0fefec4d11b731ef6870
adri <adriana03pch@gmail.com>
2024-10-28 13:03:53 -0500

    feat: add interface patient

---
+++ tukun-tech-platform/Tukun/Domain/Services/Patients/IPatientQeryService.cs
@@ -0,0 +1,12 @@
+﻿using tukun_tech_platform.Tukun.Domain.Model.Aggregates.Patients;
+using tukun_tech_platform.Tukun.Domain.Model.Queries.Patients;
+
+namespace tukun_tech_platform.Tukun.Domain.Services.Patients;
+
+public interface IPatientQeryService
+{
+    Task<Patient?> Handle(GetAllPatientsByDniQuery query);
+    Task<Patient?> Handle(GetAllPatientsByNameQuery query);
+    
+    Task<IEnumerable<Patient>> Handle(GetAllPatientsQuery query);
+}
