dce6b9380156db32a3c9d0f710577eee471edea5
adri <adriana03pch@gmail.com>
2024-10-28 12:58:53 -0500

    feat(patient): add querys

---
+++ tukun-tech-platform/Tukun/Domain/Model/Queries/Patients/GetAllPatientsByDniQuery.cs
@@ -0,0 +1,3 @@
+﻿namespace tukun_tech_platform.Tukun.Domain.Model.Queries.Patients;
+
+public record GetAllPatientsByDniQuery(string Dni);
---
+++ tukun-tech-platform/Tukun/Domain/Model/Queries/Patients/GetAllPatientsByNameQuery.cs
@@ -0,0 +1,3 @@
+﻿namespace tukun_tech_platform.Tukun.Domain.Model.Queries.Patients;
+
+public record GetAllPatientsByNameQuery(string Name,string LastName);
