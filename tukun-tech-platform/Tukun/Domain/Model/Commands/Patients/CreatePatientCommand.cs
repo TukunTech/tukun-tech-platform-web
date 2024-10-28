77ae5aed54b88bf762244693ce117f89afb5c03d
adri <adriana03pch@gmail.com>
2024-10-28 12:31:01 -0500

    feat(patient): add commands

---
+++ tukun-tech-platform/Tukun/Domain/Model/Commands/Patients/CreatePatientCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace tukun_tech_platform.Tukun.Domain.Model.Commands.Patients;
+
+public record CreatePatientCommand(string Name, string LastName, string Dni,string Gender, int Age, string BloodType, string Nationality, string NumberPolicies, string Insurance, string AlLergies);
---
+++ tukun-tech-platform/Tukun/Interfaces/REST/Transform/Patients/CreatePatientCommandFromResourceAssembler.cs
@@ -0,0 +1,10 @@
+﻿using tukun_tech_platform.Tukun.Domain.Model.Commands.Patients;
+using tukun_tech_platform.Tukun.Interfaces.REST.Resources.Patients;
+namespace tukun_tech_platform.Tukun.Interfaces.REST.Transform.Patients;
+
+public class CreatePatientCommandFromResourceAssembler 
+{
+    public static CreatePatientCommand ToCommandFromResource(CreatePatientResource resource) => new CreatePatientCommand(
+        resource.Name, resource.LastName, resource.Dni, resource.Gender, resource.Age, resource.BloodType,
+        resource.Nationality, resource.NumberPolicies, resource.Insurance, resource.AlLergies);
+}
