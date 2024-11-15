e2416f458a98a56d8895e0678a82d42494d519f4
Oscar <oscaredu01@hotmail.com>
2024-11-15 18:37:43 -0500

    feat(iam): added hashing service implementation.

---
+++ tukun-tech-platform/IAM/Infrastructure/Hashing/BCrypt/Services/HashingService.cs
@@ -0,0 +1,16 @@
+using tukun_tech_platform.IAM.Application.Internal.OutboundServices;
+using BCryptNet = BCrypt.Net.BCrypt;
+namespace tukun_tech_platform.IAM.Infrastructure.Hashing.BCrypt.Services;
+
+public class HashingService : IHashingService
+{
+    public string HashPassword(string password)
+    {
+        return BCryptNet.HashPassword(password);
+    }
+
+    public bool VerifyPassword(string password, string passwordHash)
+    {
+        return BCryptNet.Verify(password, passwordHash);
+    }
+}
